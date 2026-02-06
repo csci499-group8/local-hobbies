1b679afc869494ce4cc6e9506e8ce22c10fdb858
k-klein90 <kalliope.klein90@myhunter.cuny.edu>
2026-02-06 14:05:01 -0500

    Create backend Spring Boot project

---
+++ local-hobbies-backend/src/main/java/io/github/csci499_group8/local_hobbies/backend/LocalHobbiesBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.csci499_group8.local_hobbies.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalHobbiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalHobbiesBackendApplication.class, args);
+	}
+
+}
---
+++ local-hobbies-backend/src/test/java/io/github/csci499_group8/local_hobbies/backend/LocalHobbiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.csci499_group8.local_hobbies.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalHobbiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
